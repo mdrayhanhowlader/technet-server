c71b94fe92118e506fbd67c599914711a528a7e4
MD Rayhan Howlader <rhowlader5314@gmail.com>
2024-03-06 09:25:34 +0800

    error fixed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.euxm4cs.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k9okspq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -22,11 +22,17 @@ const run = async () => {
     const productCollection = db.collection('product');
 
     app.get('/products', async (req, res) => {
-      const cursor = productCollection.find({});
-      const product = await cursor.toArray();
-
-      res.send({ status: true, data: product });
+      try {
+        const cursor = productCollection.find({});
+        const products = await cursor.toArray();
+    
+        res.json({ status: true, data: products });
+      } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).json({ status: false, error: 'Internal Server Error' });
+      }
     });
+    
 
     app.post('/product', async (req, res) => {
       const product = req.body;
